1934325454e9035c5f19d7a619538c9061dfbf11
odudu-basi <oduduabasiav@gmail.com>
2026-03-08 14:59:28 -0500

    Initial Commit

---
+++ BeYou/BeYouApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeYouApp.swift
+//  BeYou
+//
+//  Created by Oduduabasi Victor on 3/8/26.
+//
+
+import SwiftUI
+
+@main
+struct BeYouApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BeYouTests/BeYouTests.swift
@@ -0,0 +1,17 @@
+//
+//  BeYouTests.swift
+//  BeYouTests
+//
+//  Created by Oduduabasi Victor on 3/8/26.
+//
+
+import Testing
+@testable import BeYou
+
+struct BeYouTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
